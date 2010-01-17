b59f512d5325ceb97ffa01818e8dc9060330effe
itog <itog@itog-lab.info>
2010-01-17 10:28:33 +0900

    first commit

---
+++ src/org/deb/connection/TopActivity.java
@@ -0,0 +1,41 @@
+package org.deb.connection;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class TopActivity extends Activity implements OnClickListener {
+	final String TAG = "DebCon";
+	
+	Button bluetoothButton;
+	Button handwritingButton;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		bluetoothButton = (Button)findViewById(R.id.bluetooth_button);
+		handwritingButton = (Button)findViewById(R.id.handwriting_button);
+		bluetoothButton.setOnClickListener(this);
+		handwritingButton.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.bluetooth_button:
+			//TODO start bluetooth activity
+			Log.v(TAG, "Start Bluetooth Activity");
+			break;
+		case R.id.handwriting_button:
+			//TODO start handwriting activity
+			Log.v(TAG, "Start Handwriting Activity");
+			break;
+		}
+	}
+}
